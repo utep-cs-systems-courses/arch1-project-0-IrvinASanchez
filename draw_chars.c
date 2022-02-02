c778a48f70b8de74fdf7a2c1f987466132a58b09
IrvinASanchez <iasanchez5@miners.utep.edu>
2022-02-02 11:03:32 -0700

    I added the new font

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,17 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+
+// code made for the lab
+void print_char_5x7(char c)
+{
+  c -= 0x20;
+  for (char col = 6; col > -1; col--){
+    unsigned short colMask = 1 << (6-col); /* mask to select bit associated with bit */
+    for(char row = 0; row < 5; row++) {
+      unsigned short rowBits = font_5x7[c][row];
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+  putchar('\n');
+ }
+}
